2620e3ade7fc064c146dff9e62df79f925b061b3
Victor Plushko <vicplushko33@gmail.com>
2020-04-27 05:35:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,10 @@ class Scraper
     # binding.pry
     #   end
   end
+  
+  def get_course
+    self.get_page.css(".post")
+  end
   def print_courses
     self.make_courses
     Course.all.each do |course|
